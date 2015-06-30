9eb2b9af6542838948253a0f2878b217dac9dac2
Benjamin James <benjamin-james@utulsa.edu>
2015-06-30 10:56:28 -0500

    added uds server & client

---
+++ src/client.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(int argc, char **argv)
+{
+	struct sockaddr_un addr;
+	int  sock, num;
+	char *input, buffer[256];
+
+	if (argc < 2) {
+		printf("Usage: %s [program]\n", *argv);
+		return 0;
+	}
+	sock = socket(PF_UNIX, SOCK_STREAM, 0);
+	if (sock < 0) {
+		printf("socket() failed\n");
+		return 1;
+	}
+
+	/* start with a clean addr structure */
+	memset(&addr, 0, sizeof(struct sockaddr_un));
+	addr.sun_family = AF_UNIX;
+	sprintf(addr.sun_path, "%s", argv[1]);
+
+	if (connect(sock, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) != 0) {
+		printf("connect() failed\n");
+		return 1;
+	}
+
+	rl_bind_key('\t', rl_complete);
+	while (1) {
+		snprintf(buffer, sizeof(buffer), "> ");
+		input = readline(buffer);
+		if (!input || !strcmp(input, "exit"))
+			break;
+		add_history(input);
+		write(sock, input, strlen(input));
+		free(input);
+
+		num = read(sock, buffer, sizeof(buffer));
+		buffer[num] = 0;
+		printf("%s\n", buffer);
+	}
+	num = snprintf(buffer, 256, "exit");
+	write(sock, buffer, num);
+	close(sock);
+
+	return 0;
+}
---
+++ src/server.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int connection_handler(int connection_fd)
+{
+	int nbytes = 1;
+	char buffer[256];
+	while (nbytes > 0) {
+		nbytes = read(connection_fd, buffer, 256);
+		buffer[nbytes] = 0;
+		if (!strcmp(buffer, "exit"))
+			break;
+		printf("%s\n", buffer);
+		nbytes = snprintf(buffer, 256, "%s", buffer);
+		write(connection_fd, buffer, nbytes);
+	}
+	close(connection_fd);
+	return 0;
+}
+int main(int argc, char **argv)
+{
+	struct sockaddr_un address;
+	int socket_fd, connection_fd;
+	socklen_t address_length;
+	pid_t child;
+	socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+	if (socket_fd < 0) {
+		printf("socket() failed\n");
+		return 1;
+	}
+
+	unlink(*argv);
+
+	/* start with a clean address structure */
+	memset(&address, 0, sizeof(struct sockaddr_un));
+
+	address.sun_family = AF_UNIX;
+	sprintf(address.sun_path, "%s", *argv);
+
+	if (bind(socket_fd, (struct sockaddr *) &address, sizeof(struct sockaddr_un)) != 0) {
+		printf("bind() failed\n");
+		return 1;
+	}
+
+	if (listen(socket_fd, 5) != 0) {
+		printf("listen() failed\n");
+		return 1;
+	}
+
+	while ((connection_fd = accept(socket_fd, (struct sockaddr *) &address, &address_length)) > -1) {
+		child = fork();
+		if(child == 0) {
+			/* now inside newly created connection handling process */
+			return connection_handler(connection_fd);
+		}
+
+		/* still inside server process */
+		close(connection_fd);
+	}
+
+	close(socket_fd);
+	unlink(*argv);
+	return 0;
+}
